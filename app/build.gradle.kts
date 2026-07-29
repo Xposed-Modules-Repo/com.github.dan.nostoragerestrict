cb336e49621fbdcb70526c7cffaa9b8c4a300bba
fedora-build <fedora-build@example.com>
2026-07-29 19:47:28 -0300

    build: bump version to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.dan.NoStorageRestrict"
         minSdk = 15
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6.0"
+        versionCode = 7
+        versionName = "0.7.0"
 
         vectorDrawables.generatedDensities?.clear()
     }
